4d9876bb5a3476630745f75ffed4cd024eaf84ab
Adrian <adriangarciabernardos@gmail.com>
2022-01-19 19:35:42 +0100

    transitions and button optimizated

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const list = document.querySelector(".menu-list");
-const input = document.querySelector('#icon');
-const trueInput = document.querySelector('.fas')
-const body = document.body
-input.addEventListener('click', function(){
-        list.classList.toggle('show-list')
-        if(trueInput.className.includes('fa-bars')){
-            trueInput.classList.replace('fa-bars','fa-times')
-            body.style.overflow = 'hidden'
-        }
-        else{
-            trueInput.classList.replace('fa-times','fa-bars')
-            body.style.overflow = 'scroll'
-        }
-
-})
+document.querySelector('.btn').onclick = function (e){
+    const body = document.body
+    var menu = document.querySelector('.menu')
+    var btn = document.querySelector('.btn')
+    menu.classList.toggle('is-active')
+    btn.classList.toggle('is-active')
+    e.preventDefault()
+    if(menu.className.includes('is-active')){
+        document.body.style.overflow = 'hidden'
+    }
+    else{
+        document.body.style.overflow = 'scroll'
+    }
+} 
